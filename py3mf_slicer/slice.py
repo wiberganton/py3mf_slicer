64721c5d5d0fbd480bfc3244c42bdee634dd0eff
Anton Wiberg <wiberg.anton@gmail.com>
2024-09-18 16:25:41 +0200

    minor update

--- py3mf_slicer/slice.py
+++ py3mf_slicer/slice.py
@@ -1,6 +1,6 @@
 import lib3mf
 import pyvista as pv
-from py3mf_slicer.get_items import get_pyvista_mesh
+from py3mf_slicer.get_items import get_pyvista_meshes
 from ctypes import c_float, c_uint32
 import numpy as np
 import networkx as nx
